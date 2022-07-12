0b983bc7453476e585c44abd6ce1cfd597fb1809
zbansfield <zainab_bansfield@intuit.com>
2022-07-12 13:31:08 -0700

    Search bar works and url updates

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React, { Component, useEffect, useState } from 'react';
 import './App.css';
 import apiKey from './config';
 import { BrowserRouter, Route, Link, NavLink, Routes } from 'react-router-dom';
+// import createBrowserHistory from 'history/createBrowserHistory'
 
 import Search from './Search';
 import Nav from './Nav';
 import PhotoContainer from './PhotoContainer';
 
 import axios from 'axios';
+
+var createBrowserHistory = require("history").createBrowserHistory
+
 
 
 export default function App() {
@@ -26,55 +30,27 @@ export default function App() {
       });
   }, [category])
 
-  // const changeCategory = (input = 'cats') => {
-  //   setCategory(input);
-  // }
+  return (
+    <div className="container">
+      <Search changeCategory={category => setCategory(category)} history={createBrowserHistory()}/>
 
-  // componentDidMount() {
-  //   fetch(`https://api.flickr.com/services/rest/?format=json&method=flickr.photos.search&api_key=${apiKey}&text=birds&per_page=24&nojsoncallback=?`)
-  //   .then(res => res.json())
-  //   .then(data => {
-  //       this.setState({
-  //         photos: data.photos.photo
-  //       })
-  //   })
-  //   .catch(error => {
-  //   console.log('Error fetching and parsing data', error);
-  //   });
-  // }
+      <nav className="main-nav">
+        <ul>
+            <Nav category='birds' changeCategory={category => setCategory(category)}/>
+            <Nav category='books' changeCategory={category => setCategory(category)}/>
+            <Nav category='trees' changeCategory={category => setCategory(category)}/>
+        </ul>
+      </nav>
 
-  // search = (category) => {
-  //   fetch(`https://api.flickr.com/services/rest/?format=json&method=flickr.photos.getRecent&api_key=${apiKey}&text=${category}&per_page=24&nojsoncallback=?`)
-  //       .then(res => res.json())
-  //       .then(data => {
-  //           this.setState({
-  //             photos: data.photos.photo
-  //           })
-  //       })
-  //       .catch(error => {
-  //       console.log('Error fetching and parsing data', error);
-  //       });
-  // }
-
-    return (
-      <div className="container">
-        <Search />
-        <nav className="main-nav">
-          <ul>
-              <Nav category='birds' changeCategory={category => setCategory(category)}/>
-              <Nav category='books' changeCategory={category => setCategory(category)}/>
-              <Nav category='trees' changeCategory={category => setCategory(category)}/>
-          </ul>
-        </nav>
-
-        <Routes>
-          <Route path='/birds' element={<PhotoContainer category={category} photos={photos}/>}/>
-          <Route path='/trees' element={<PhotoContainer category={category} photos={photos}/>}/>
-          <Route path='/books' element={<PhotoContainer category={category} photos={photos}/>}/>
-        </Routes>
-        {/* <Nav apiKey={apiKey} changeCategory={category => {setCategory(category)}} photos={photos}/> */}
-        {/* <Routes>
-        </Routes> */}
-      </div>
-    );
+      <Routes>
+        <Route path='/birds' element={<PhotoContainer category={category} photos={photos}/>}/>
+        <Route path='/books' element={<PhotoContainer category={category} photos={photos}/>}/>
+        <Route path='/trees' element={<PhotoContainer category={category} photos={photos}/>}/>
+        <Route path='/:category' element={<PhotoContainer category={category} photos={photos}/>}/>
+      </Routes>
+      {/* <Nav apiKey={apiKey} changeCategory={category => {setCategory(category)}} photos={photos}/> */}
+      {/* <Routes>
+      </Routes> */}
+    </div>
+  );
 }
--- src/Search.js
+++ src/Search.js
@@ -2,36 +2,9 @@ import React, { Component } from "react";
 
 class Search extends Component {
 
-    // state = {
-    //     searchText: '',
-    //     photos: []
-    // }
-    
-    // componentDidMount() {
-    //     fetch(`https://api.flickr.com/services/rest/?format=json&method=flickr.photos.search&api_key=${this.props.apiKey}&text=birds&per_page=24&nojsoncallback=?`)
-    //         .then(res => res.json())
-    //         .then(data => {
-    //             this.setState({
-    //                 photos: data.photos.photo
-    //             })
-    //         })
-    //         .catch(error => {
-    //         console.log('Error fetching and parsing data', error);
-    //         });
-    // }
-    
-    //   search = (category) => {
-    //     fetch(`https://api.flickr.com/services/rest/?format=json&method=flickr.photos.getRecent&api_key=${apiKey}&text=${category}&per_page=24&nojsoncallback=?`)
-    //         .then(res => res.json())
-    //         .then(data => {
-    //             this.setState({
-    //               photos: data.photos.photo
-    //             })
-    //         })
-    //         .catch(error => {
-    //         console.log('Error fetching and parsing data', error);
-    //         });
-    //   }
+    state = {
+        searchText: ''
+    }
     
     onSearchChange = e => {
         this.setState({ searchText: e.target.value });
@@ -39,14 +12,16 @@ class Search extends Component {
 
     handleSubmit = e => {
         e.preventDefault();
-        this.props.onSearch(this.category.value)
+        this.props.changeCategory(this.query.value)
+        let searched = this.query.value
         e.currentTarget.reset();
+        this.props.history.push(`/${searched}`)
     }
     
     render() {
         return (
             <form className="search-form" onSubmit={this.handleSubmit}>
-                <input type="search" name="search" placeholder="Search" required onChange={this.onSearchChange} ref={ (input) => this.category = input }/>
+                <input type="search" name="search" placeholder="Search" required onChange={this.onSearchChange} ref={(input) => this.query = input} />
                 <button type="submit" className="search-button">
                     <svg fill="#fff" height="24" viewBox="0 0 23 23" width="24" xmlns="http://www.w3.org/2000/svg">
                         <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
